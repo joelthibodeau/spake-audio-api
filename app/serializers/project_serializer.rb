f66a2cf4d735f77347b811871c886002914e8fc4
joelthibodeau <joel.thibodeau@gmail.com>
2019-01-15 16:28:24 -0500

    Add "# frozen_string_literal: true" to top of serializer files.

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :sessions
 end
--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class SessionSerializer < ActiveModel::Serializer
   attributes :id, :date, :hours_worked, :hours_recorded, :notes
 end
