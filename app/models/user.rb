8b1e80474bf69b863174cbfd3297018f58d1547e
joelthibodeau <joel.thibodeau@gmail.com>
2019-01-17 14:59:34 -0500

    Fix session's relationship with Project and user Additional: -  add 'has_many :sessions, through: :projects' relationship to    user model - add ':project_id' to session_serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   include Authentication
   has_many :examples
   has_many :projects
+  has_many :sessions, through: :projects
 
-  # have a collection of sessions that belongs to user
-  def sessions
-    # find all the projects
-    # find all sessions that belong to projects
-    self.projects.map { |project| project.sessions }
-  end
+  # # have a collection of sessions that belongs to user
+  # def sessions
+  #   # find all the projects
+  #   # find all sessions that belong to projects
+  #   self.projects.map { |project| project.sessions }
+  # end
 end
--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class SessionSerializer < ActiveModel::Serializer
-  attributes :id, :date, :notes, :hours_worked_readable, :hours_recorded_readable
+  attributes :id, :date, :notes, :hours_worked_readable,
+             :hours_recorded_readable, :project_id
 
   def hours_worked_readable
     object.hours_worked.to_f
